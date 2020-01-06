aafe86cf7c33cd90cfd201a87e4b796d625ed36d
VTSquirrel <slutskda@vt.edu>
2020-01-05 20:19:51 -0500

    Properly format phone numbers and remove trailing whitespace from extracted data

--- conversion.php
+++ conversion.php
@@ -51,17 +51,17 @@
 				    }
 
 				    if(preg_match('/(\d[\s-]?)?[\(\[\s-]{0,2}?\d{3}[\)\]\s-]{0,2}?\d{3}[\s-]?\d{4}/i',$line, $matches)){
-				      	$phone = $matches[0];
+				      	$phone = $this->formatPhoneNumber($matches[0]);
 				    }
 
 				    if (preg_match("/[a-z\d._%+-]+@[a-z\d.-]+\.[a-z]{2,4}\b/i", $line, $match)){
-				       	$email = $match[0];
+				       	$email = trim($match[0], " ");
 				    }
 
 					$name = $this->search($line);
 					if (count($name) > 0 && empty($fname) && empty($lname) && $count <= 3){
-						$fname = $name[0];
-						$lname = $name[1];
+						$fname = trim($name[0], " ");
+						$lname = trim($name[1], " ");
 					}
 			    }
 			    
@@ -136,6 +136,35 @@
 		    $bom = pack('H*','EFBBBF');
 		    $text = preg_replace("/^$bom/", '', $text);
 		    return $text;
+		}
+
+		/* Courtesy of https://stackoverflow.com/a/14167216 */
+		private function formatPhoneNumber($phoneNumber) {
+			$phoneNumber = preg_replace('/[^0-9]/','',$phoneNumber);
+
+			if(strlen($phoneNumber) > 10) {
+				$countryCode = substr($phoneNumber, 0, strlen($phoneNumber)-10);
+				$areaCode = substr($phoneNumber, -10, 3);
+				$nextThree = substr($phoneNumber, -7, 3);
+				$lastFour = substr($phoneNumber, -4, 4);
+
+				$phoneNumber = '+'.$countryCode.' ('.$areaCode.') '.$nextThree.'-'.$lastFour;
+			}
+			else if(strlen($phoneNumber) == 10) {
+				$areaCode = substr($phoneNumber, 0, 3);
+				$nextThree = substr($phoneNumber, 3, 3);
+				$lastFour = substr($phoneNumber, 6, 4);
+
+				$phoneNumber = '('.$areaCode.') '.$nextThree.'-'.$lastFour;
+			}
+			else if(strlen($phoneNumber) == 7) {
+				$nextThree = substr($phoneNumber, 0, 3);
+				$lastFour = substr($phoneNumber, 3, 4);
+
+				$phoneNumber = $nextThree.'-'.$lastFour;
+			}
+
+			return $phoneNumber;
 		}
 	}
 ?>
